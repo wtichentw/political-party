b35f887bc531d90fb6fd094f546630a8d31694a1
samick17 <boneache@gmail.com>
2016-01-03 17:00:42 +0800

    scale-up character by time

--- js/boot.js
+++ js/boot.js
@@ -15,6 +15,6 @@ Boot.prototype = {
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
 
-    game.state.start("Intro");
+    game.state.start("FlappyPig");
   }
 };
