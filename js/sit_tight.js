2e8cbc164834f816662736821f4ae6c92b49b26d
Harry Chang <harry830622@gmail.com>
2016-01-07 10:20:32 +0800

    Create the initial screen
    Refractor the code with the function in utils.js.

--- js/sit_tight.js
+++ js/sit_tight.js
@@ -1,169 +1,157 @@
-SitTightGame = function(game) {};
+var SitTightGame = function(game) {
+  this.game = game;
+};
 
 (function() {
-  var keys = [
-    {
-      text: "A",
-      key: Phaser.KeyCode.A
-    },
-    {
-      text: "B",
-      key: Phaser.KeyCode.B
-    },
-    {
-      text: "C",
-      key: Phaser.KeyCode.C
-    },
-    {
-      text: "D",
-      key: Phaser.KeyCode.D
-    },
-    {
-      text: "E",
-      key: Phaser.KeyCode.E
-    },
-    {
-      text: "F",
-      key: Phaser.KeyCode.F
-    },
-    {
-      text: "G",
-      key: Phaser.KeyCode.G
-    },
-    {
-      text: "H",
-      key: Phaser.KeyCode.H
-    },
-    {
-      text: "I",
-      key: Phaser.KeyCode.I
-    },
-    {
-      text: "J",
-      key: Phaser.KeyCode.J
-    },
-    {
-      text: "K",
-      key: Phaser.KeyCode.K
-    },
-    {
-      text: "L",
-      key: Phaser.KeyCode.L
-    },
-    {
-      text: "M",
-      key: Phaser.KeyCode.M
-    },
-    {
-      text: "N",
-      key: Phaser.KeyCode.N
-    },
-    {
-      text: "O",
-      key: Phaser.KeyCode.O
-    },
-    {
-      text: "P",
-      key: Phaser.KeyCode.P
-    },
-    {
-      text: "Q",
-      key: Phaser.KeyCode.Q
-    },
-    {
-      text: "R",
-      key: Phaser.KeyCode.R
-    },
-    {
-      text: "S",
-      key: Phaser.KeyCode.S
-    },
-    {
-      text: "T",
-      key: Phaser.KeyCode.T
-    },
-    {
-      text: "U",
-      key: Phaser.KeyCode.U
-    },
-    {
-      text: "V",
-      key: Phaser.KeyCode.V
-    },
-    {
-      text: "W",
-      key: Phaser.KeyCode.W
-    },
-    {
-      text: "X",
-      key: Phaser.KeyCode.X
-    },
-    {
-      text: "Y",
-      key: Phaser.KeyCode.Y
-    },
-    {
-      text: "Z",
-      key: Phaser.KeyCode.Z
-    }
-  ];
+  "use strict";
 
-  var blahTexts = [
-    "都快坐到桌子下面了啦",
-    "屁股坐的好酸喔",
-    "我說過會坐好坐滿嘛",
-    "好想站起來休息一下",
-    "啊唷！真拿你沒辦法",
-    "這麼雷的遊戲你也敢玩？",
-    "有必要這樣逼人家坐著嗎？",
-    "我要使出全力站起來囉",
-    "淡水阿嬤在找我啦！",
-    "啊啊啊！快站起來了！"
-  ];
+  // var blahTexts = [
+  //   "都快坐到桌子下面了啦",
+  //   "屁股坐的好酸喔",
+  //   "我說過會坐好坐滿嘛",
+  //   "好想站起來休息一下",
+  //   "啊唷！真拿你沒辦法",
+  //   "這麼雷的遊戲你也敢玩？",
+  //   "有必要這樣逼人家坐著嗎？",
+  //   "我要使出全力站起來囉",
+  //   "淡水阿嬤在找我啦！",
+  //   "啊啊啊！快站起來了！"
+  // ];
 
-  var grandmaWords =
-    "記得有一次到淡水去拜拜的時候\n\
-    一個阿嬤跟我說：\n\
-    「市長，你一定要出來選。」\n\
-    我說：「我已經承諾所有辛北市民\n\
-    阿嬤跟我說：「你如果不出來，\n\
-    連天公伯都不會原諒你。」\n\
-    這段話讓我非常感動！";
-
-  var bgm;
-  var grandmaMusic;
-
-  var imagePig;
-
-  var keyText;
-  var keysIdx;
-
-  var introLayer;
-
-  var isStageStarted;
-  var isTimerStarted;
-
-  var sitSpeed = 1;
-
-  var isBlahLayerExist;
+  // var grandmaWords = "記得有一次到淡水去拜拜的時候\n一個阿嬤跟我說：\n「市長，你一定要出來選。」\n我說：「我已經承諾所有辛北市民\n阿嬤跟我說：「你如果不出來，\n連天公伯都不會原諒你。」\n這段話讓我非常感動！";
 
   SitTightGame.prototype = {
 
+    images: {
+      bg: {
+        path: "/media/sit_tight/bg.png",
+        key: "SitTightBg",
+        image: {}
+      },
+      pig: {
+        path: "/media/sit_tight/pig.png",
+        key: "SitTightPig",
+        image: {}
+      },
+      desk: {
+        path: "/media/sit_tight/desk.png",
+        key: "SitTightDesk",
+        image: {}
+      }
+    },
+
+    draw: {
+      bg: function(game) {
+        var imageBg = game.add.image(0, 0, "SitTightBg");
+
+        return imageBg;
+      },
+      pig: function(game) {
+        var imagePig = game.add.image(gameWidth / 2, gameHeight + 200, "SitTightPig");
+        imagePig.anchor.set(0.5, 1);
+
+        return imagePig;
+      },
+      desk: function(game) {
+        var imageDesk = game.add.image(gameWidth / 2, gameHeight, "SitTightDesk");
+        imageDesk.anchor.set(0.5, 1);
+
+        return imageDesk;
+      },
+      keyboard: function(game, text) {
+        var keyboardWidth = 70;
+        var keyboardHeight = keyboardWidth;
+        var keyboardX = 100;
+        var keyboardY = 100;
+
+        var graphics = game.add.graphics(0, 0);
+        graphics.beginFill(0x333333);
+        graphics.drawRoundedRect(
+          keyboardX,
+          keyboardY,
+          keyboardWidth,
+          keyboardHeight,
+          10
+        );
+        graphics.endFill();
+
+        var keyText = game.add.text(
+          keyboardX + keyboardWidth / 2,
+          keyboardY + keyboardHeight / 2,
+          text,
+          {
+            font: "40px Arial",
+            fill: "#FFFFFF"
+          }
+        );
+        keyText.anchor.set(0.5);
+
+        var layer = game.add.group();
+        layer.add(graphics);
+        layer.add(keyText);
+
+        return layer;
+      }
+    },
+
+    keyboard: {
+      keys: {
+        "A": Phaser.KeyCode.A,
+        "B": Phaser.KeyCode.B,
+        "C": Phaser.KeyCode.C,
+        "D": Phaser.KeyCode.D,
+        "E": Phaser.KeyCode.E,
+        "F": Phaser.KeyCode.F,
+        "G": Phaser.KeyCode.G,
+        "H": Phaser.KeyCode.H,
+        "I": Phaser.KeyCode.I,
+        "J": Phaser.KeyCode.J,
+        "K": Phaser.KeyCode.K,
+        "L": Phaser.KeyCode.L,
+        "M": Phaser.KeyCode.M,
+        "N": Phaser.KeyCode.N,
+        "O": Phaser.KeyCode.O,
+        "P": Phaser.KeyCode.P,
+        "Q": Phaser.KeyCode.Q,
+        "R": Phaser.KeyCode.R,
+        "S": Phaser.KeyCode.S,
+        "T": Phaser.KeyCode.T,
+        "U": Phaser.KeyCode.U,
+        "V": Phaser.KeyCode.V,
+        "W": Phaser.KeyCode.W,
+        "X": Phaser.KeyCode.X,
+        "Y": Phaser.KeyCode.Y,
+        "Z": Phaser.KeyCode.Z
+      },
+
+      nextKey: function() {
+        var keysKeys = Object.keys(this.keys);
+        var keyText = keysKeys[game.rnd.between(0, keysKeys.length - 1)];
+        return {
+          text: keyText,
+          key: this.keys[keyText]
+        };
+      },
+
+      currentKey: {
+        text: "A",
+        key: Phaser.KeyCode.A
+      }
+    },
+
     preload: function() {
 
-      game.load.audio("SitTightBGM", "media/sit_tight/bgm.mp3");
-      game.load.audio("SitTightGrandmaMusic", "media/sit_tight/grandma_music.mp3");
+      // game.load.audio("SitTightBGM", this.background.auidioPath);
+      // game.load.audio("SitTightGrandmaMusic", "media/sit_tight/grandma_music.mp3");
 
-      game.load.image("SitTightPig", "media/sit_tight/pig.png");
-      game.load.image("SitTightBg", "media/sit_tight/bg.png");
-      game.load.image("SitTightDesk", "media/sit_tight/desk.png");
-      game.load.image("SitTightDialog", "media/sit_tight/dialog.png");
-      game.load.image("SitTightGrandma", "media/sit_tight/grandma.png");
-
-      keysIdx = game.rnd.between(0, keys.length - 1);
-
-      isStageStarted = false;
-      isTimerStarted = false;
+      game.load.image(this.images.pig.key, this.images.pig.path);
+      game.load.image(this.images.bg.key, this.images.bg.path);
+      game.load.image(this.images.desk.key, this.images.desk.path);
+      // game.load.image("SitTightDialog", "media/sit_tight/dialog.png");
+      // game.load.image("SitTightGrandma", "media/sit_tight/grandma.png");
+      // game.load.image("SitTightExplanation1", this.explanations.imagePaths[0]);
+      // game.load.image("SitTightExplanation2", this.explanations.imagePaths[1]);
+      // game.load.image("SitTightExplanation3", this.explanations.imagePaths[2]);
 
     },
 
@@ -171,220 +159,159 @@ SitTightGame = function(game) {};
 
       game.stage.setBackgroundColor(0xFFFFFF);
 
-      bgm = game.add.audio("SitTightBGM", 1, true);
-      bgm.play();
-      grandmaMusic = game.add.audio("SitTightGrandmaMusic");
+      // this.background.audio = game.add.audio("SitTightBGM", 1, true);
+      // grandmaMusic = game.add.audio("SitTightGrandmaMusic");
 
-      // Draw background
-      var imageBg = game.add.image(0, 0, "SitTightBg");
+      // this.background.audio.play();
 
-      var bgLayer = game.add.group();
-      bgLayer.add(imageBg);
-      bgLayer.z = 0;
+      this.images.bg.image = this.draw.bg(this.game);
+      this.images.pig.image = this.draw.pig(this.game);
+      this.images.desk.image = this.draw.desk(this.game);
 
-      // Draw Mr.Pig
-      var scaleFactor = 0.5;
-      imagePig = game.add.image(gameWidth / 2, gameHeight + 200, "SitTightPig");
-      imagePig.scale.set(scaleFactor);
-      imagePig.anchor.set(0.5, 1)
-
-      var pigLayer = game.add.group();
-      pigLayer.add(imagePig);
-      pigLayer.z = 1;
-
-      // Draw desk
-      var imageDesk = game.add.image(gameWidth / 2, gameHeight, "SitTightDesk");
-      imageDesk.anchor.set(0.5, 1);
-
-      var deskLayer = game.add.group();
-      deskLayer.add(imageDesk);
-      deskLayer.z = 2;
-
-      // Draw HUD
-      var keyboardWidth = 70;
-      var keyboardHeight = keyboardWidth;
-      var keyboardX = 100;
-      var keyboardY = 100;
-      keyText = game.add.text(
-        keyboardX + keyboardWidth / 2,
-        keyboardY + keyboardHeight / 2,
-        keys[keysIdx].text,
-        {
-          font: "40px Arial",
-          fill: "#FFFFFF"
-        }
+      drawLayersInOrder(
+        [
+          [this.images.bg.image],
+          [this.images.pig.image],
+          [this.images.desk.image],
+          this.draw.keyboard(this.game, this.keyboard.currentKey.text)
+        ],
+        this.game
       );
-      keyText.anchor.set(0.5);
-
-      graphicsHUD = game.add.graphics(0, 0);
-
-      graphicsHUD.beginFill(0x333333);
-      graphicsHUD.drawRoundedRect(
-        keyboardX,
-        keyboardY,
-        keyboardWidth,
-        keyboardHeight,
-        10
-      );
-      graphicsHUD.endFill();
-
-      var HUDLayer = game.add.group();
-      HUDLayer.add(graphicsHUD);
-      HUDLayer.add(keyText);
-      HUDLayer.z = 3;
-
-      // Draw intro screen
-      var graphicsIntro = game.add.graphics(0, 0);
-
-      graphicsIntro.beginFill(0x000000, 0.5);
-      graphicsIntro.drawRect(0, 0, gameWidth, gameHeight);
-      graphicsIntro.endFill();
-
-      introLayer = game.add.group();
-      introLayer.add(graphicsIntro);
-      introLayer.z = 4;
 
     },
 
     update: function() {
 
-      if (game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)) {
-        introLayer.destroy();
-        isStageStarted = true;
-      }
+      //       if (isStageStarted) {
 
-      if (isStageStarted) {
+      //         if (!isTimerStarted) {
+      //           var KEY_CHANGE_INTERVAL = 2000;
+      //           game.time.events.loop(KEY_CHANGE_INTERVAL, function() {
+      //             keysIdx = game.rnd.between(0, keys.length - 1);
+      //             keyText.setText(keys[keysIdx].text);
 
-        if (!isTimerStarted) {
-          var KEY_CHANGE_INTERVAL = 2000;
-          game.time.events.loop(KEY_CHANGE_INTERVAL, function() {
-            keysIdx = game.rnd.between(0, keys.length - 1);
-            keyText.setText(keys[keysIdx].text);
+      //             var BLAH_TEXT_X = imagePig.x + 400;
+      //             var BLAH_TEXT_Y = 100;
+      //             var blahText = game.add.text(
+      //               BLAH_TEXT_X,
+      //               BLAH_TEXT_Y,
+      //               blahTexts[game.rnd.between(0, blahTexts.length - 1)],
+      //               {
+      //                 font: "40px Arial",
+      //                 fill: "#000000"
+      //               }
+      //             );
+      //             blahText.anchor.set(0.5);
 
-            var BLAH_TEXT_X = imagePig.x + 400;
-            var BLAH_TEXT_Y = 100;
-            var blahText = game.add.text(
-              BLAH_TEXT_X,
-              BLAH_TEXT_Y,
-              blahTexts[game.rnd.between(0, blahTexts.length - 1)],
-              {
-                font: "40px Arial",
-                fill: "#000000"
-              }
-            );
-            blahText.anchor.set(0.5);
+      //             var dialogImage = game.add.image(blahText.x, blahText.y, "SitTightDialog");
+      //             dialogImage.anchor.set(0.5);
 
-            var dialogImage = game.add.image(blahText.x, blahText.y, "SitTightDialog");
-            dialogImage.anchor.set(0.5);
+      //             var blahLayer = game.add.group();
+      //             blahLayer.add(dialogImage);
+      //             blahLayer.add(blahText);
+      //             blahLayer.z = 5;
 
-            var blahLayer = game.add.group();
-            blahLayer.add(dialogImage);
-            blahLayer.add(blahText);
-            blahLayer.z = 5;
+      //             var BLAH_DISAPPEAR_INTERVAL = 1500;
+      //             game.time.events.add(BLAH_DISAPPEAR_INTERVAL, function() {
+      //               blahLayer.destroy();
+      //             });
 
-            isBlahLayerExist = true;
-            var BLAH_DISAPPEAR_INTERVAL = 1500;
-            game.time.events.add(BLAH_DISAPPEAR_INTERVAL, function() {
-              blahLayer.destroy();
-              isBlahLayerExist = false;
-            });
+      //           });
 
-          });
+      //           var warningText = game.add.text(
+      //             gameWidth / 2,
+      //             gameHeight / 2,
+      //             "WARNING",
+      //             {
+      //               font: "200px Arial",
+      //               fill: "#FF0000"
+      //             }
+      //           );
+      //           warningText.alpha = 0.0;
+      //           warningText.anchor.set(0.5);
+      //           var GRANDMA_INTERVAL = 10000;
+      //           game.time.events.add(GRANDMA_INTERVAL, function() {
+      //             var warningTextTween = game.add.tween(warningText).to(
+      //               {
+      //                 alpha: 1
+      //               },
+      //               200,
+      //               "Linear",
+      //               true,
+      //               0,
+      //               10,
+      //               true
+      //             );
+      //             warningTextTween.onComplete.add(function() {
+      //               var imageGrandma = game.add.image(0, gameHeight, "SitTightGrandma");
+      //               imageGrandma.scale.set(0.5);
+      //               imageGrandma.anchor.set(0.5, 1);
+      //               var imageGrandmaTween = game.add.tween(imageGrandma).to(
+      //                 {
+      //                   x: gameWidth + imageGrandma.width / 2
+      //                 },
+      //                 5000,
+      //                 "Linear",
+      //                 true
+      //               );
+      //               imageGrandmaTween.onComplete.add(function() {
+      //                 game.time.removeAll();
+      //                 isStageStarted = false;
+      //                 var graphics = game.add.graphics(0, 0);
+      //                 graphics.beginFill(0x000000, 0.5);
+      //                 graphics.drawRect(0, 0, gameWidth, gameHeight);
+      //                 graphics.endFill();
+      //                 var grandmaText = game.add.text(
+      //                   gameWidth / 2,
+      //                   gameHeight,
+      //                   grandmaWords,
+      //                   {
+      //                     font: "60px Arial",
+      //                     fill: "#FFFFFF"
+      //                   }
+      //                 );
+      //                 grandmaText.anchor.set(0.5, 1);
+      //                 bgm.pause();
+      //                 grandmaMusic.play();
+      //                 var grandmaTextTween = game.add.tween(grandmaText).to(
+      //                   {
+      //                     y: 0
+      //                   },
+      //                   15000,
+      //                   "Linear",
+      //                   true
+      //                 );
+      //                 grandmaTextTween.onComplete.add(function() {
+      //                   graphics.clear();
+      //                   grandmaMusic.stop();
+      //                   bgm.resume();
+      //                   imagePig.y -= 200;
+      //                   isStageStarted = true;
+      //                   isTimerStarted = false;
+      //                 }, grandmaTextTween);
+      //               }, imageGrandmaTween);
+      //             }, warningTextTween);
+      //           });
 
-          var warningText = game.add.text(
-            gameWidth / 2,
-            gameHeight / 2,
-            "WARNING",
-            {
-              font: "200px Arial",
-              fill: "#FF0000"
-            }
-          );
-          warningText.alpha = 0.0;
-          warningText.anchor.set(0.5);
-          var GRANDMA_INTERVAL = 10000;
-          game.time.events.add(GRANDMA_INTERVAL, function() {
-            var warningTextTween = game.add.tween(warningText).to(
-              {
-                alpha: 1
-              },
-              200,
-              "Linear",
-              true,
-              0,
-              10,
-              true
-            );
-            warningTextTween.onComplete.add(function() {
-              var imageGrandma = game.add.image(0, gameHeight, "SitTightGrandma");
-              imageGrandma.scale.set(0.5);
-              imageGrandma.anchor.set(0.5, 1);
-              var imageGrandmaTween = game.add.tween(imageGrandma).to(
-                {
-                  x: gameWidth + imageGrandma.width / 2
-                },
-                5000,
-                "Linear",
-                true
-              );
-              imageGrandmaTween.onComplete.add(function() {
-                game.time.removeAll();
-                isStageStarted = false;
-                var graphics = game.add.graphics(0, 0);
-                graphics.beginFill(0x000000, 0.5);
-                graphics.drawRect(0, 0, gameWidth, gameHeight);
-                graphics.endFill();
-                var grandmaText = game.add.text(
-                  gameWidth / 2,
-                  gameHeight,
-                  grandmaWords,
-                  {
-                    font: "60px Arial",
-                    fill: "#FFFFFF"
-                  }
-                );
-                grandmaText.anchor.set(0.5, 1);
-                bgm.pause();
-                grandmaMusic.play();
-                var grandmaTextTween = game.add.tween(grandmaText).to(
-                  {
-                    y: 0
-                  },
-                  15000,
-                  "Linear",
-                  true
-                );
-                grandmaTextTween.onComplete.add(function() {
-                  graphics.clear();
-                  grandmaMusic.stop();
-                  bgm.resume();
-                  imagePig.y -= 200;
-                  isStageStarted = true;
-                  isTimerStarted = false;
-                }, grandmaTextTween);
-              }, imageGrandmaTween);
-            }, warningTextTween);
-          });
+      //           isTimerStarted = true;
+      //         }
 
-          isTimerStarted = true;
-        }
+      //         if (imagePig.y >= gameHeight) {
+      //           imagePig.y -= sitSpeed;
+      //         } else {
+      //           alert("請坐好，坐滿");
+      //           bgm.stop();
+      //           game.state.start("MainMenu");
+      //           return;
+      //         }
 
-        if (imagePig.y >= gameHeight) {
-          imagePig.y -= sitSpeed;
-        } else {
-          alert("請坐好，坐滿");
-          bgm.stop();
-          game.state.start("MainMenu");
-          return;
-        }
+      //         if (game.input.keyboard.isDown(keys[keysIdx].key) && imagePig.y < gameHeight + 400) {
+      //           imagePig.y += (sitSpeed + 1);
+      //         }
 
-        if (game.input.keyboard.isDown(keys[keysIdx].key) && imagePig.y < gameHeight + 400) {
-          imagePig.y += (sitSpeed + 1);
-        }
-
-      }
+      //       }
     }
 
   };
 
-}());
+})();
